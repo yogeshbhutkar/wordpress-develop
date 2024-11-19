af0dfcc23f1f4aa704d2c27c29b347f61346b4e2
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2024-11-19 09:37:32 +0530

    Nav Menu: Add translator comments for menu and submenu item numbering

--- src/js/_enqueues/lib/nav-menu.js
+++ src/js/_enqueues/lib/nav-menu.js
@@ -364,6 +364,7 @@
 									if ( i == itemPosition ) { 
 										$selected = 'selected';
 									}
+									// Translators: %1$s is the current menu item number, %2$s is the total number of menu items.
 									$html += '<option ' + $selected + ' value="' + i + '">' + wp.i18n.sprintf( wp.i18n._x( '%1$s of %2$s', 'indicating a part of a total number of items in a navigation menu' ), i, totalMenuItems ) + '</option>';
 								}
 
@@ -380,6 +381,7 @@
 									if ( i == itemPosition ) {
 										$selected = 'selected';
 									}
+									// Translators: %1$s is the current submenu item number, %2$s is the total number of submenu items.
 									$html += '<option ' + $selected + ' value="' + i + '">' + wp.i18n.sprintf( wp.i18n._x( '%1$s of %2$s', 'indicating a part of a total number of items in the submenu' ), i, totalSubMenuItems ) + '</option>';
 								}
 
